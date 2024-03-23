7c3c7f79b1cf488df9729cfc49cced1db9c5c4a4
Ved K <vedkorla300@gmail.com>
2024-03-23 11:23:49 +0530

    Modifications 23rd March 2024

--- publisher.py
+++ publisher.py
@@ -1,49 +1,43 @@
 import paho.mqtt.client as mqtt
 import time
 
-# constants
 MQTT_BROKER = "localhost"  # broker address
 MQTT_PORT = 1883  # mqtt port
 TOPIC = "test/code"
 BACK_CHANNEL = "test/output"
 
-# c++ code to print "Hello, world!"
 cpp_code = """
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+int a=2;
+int b=4;
+    cout << "Hello, world!" << endl;
+   cout << a + b << endl;
     return 0;
 }
 """
 
-# callback function when the publisher connects to the broker
 def on_connect(client, userdata, flags, rc):
     print("Publisher connected with result code "+str(rc))
     publisher.subscribe(BACK_CHANNEL)
     print(f"Subsribed to {BACK_CHANNEL}")
 
 
-# callback function when the publisher receives acknowledgment
 def on_message(client, userdata, msg):
     print("Received acknowledgment from subscriber:")
     print(msg.payload.decode())
 
-# Create MQTT client instance
 publisher = mqtt.Client("Publisher")
 
-# Set up callback functions
 publisher.on_connect = on_connect
 publisher.on_message = on_message
 
-# Connect to MQTT broker
 publisher.connect(MQTT_BROKER, MQTT_PORT, 60)
 publisher.loop_start()
-# Subscribe to the acknowledgment topic
 
 
-# Start the MQTT client loop
 while True:
-     # Publish the C++ code to the subscriber
     publisher.publish(TOPIC, cpp_code)
     print(f"Code published\nWaiting for output")
     time.sleep(5)
--- subscriber.py
+++ subscriber.py
@@ -1,13 +1,11 @@
 import paho.mqtt.client as mqtt
 import subprocess
 
-# Define constants
 MQTT_BROKER = "localhost"  # broker address
 MQTT_PORT = 1883  # mqtt port
 TOPIC = "test/code"
 BACK_CHANNEL = "test/output"
 
-# Callback function when the subscriber receives a message
 def on_message(client, userdata, msg):
     print("Received code from publisher:")
     print(msg.payload.decode())
@@ -23,21 +21,17 @@ def on_connect(client, userdata, flags, rc):
     print(f"Connected with code {rc}")
 
 def execute_cpp_code(code):
-    # Write the C++ code to a file
     with open("temp.cpp", "w") as file:
         file.write(code)
 
-    # Compile the C++ code
     subprocess.run(["g++", "temp.cpp", "-o", "temp.out"])
 
-    # Execute the compiled binary and capture the output
     result = subprocess.run(["./temp.out"], capture_output=True, text=True)
 
     return result.stdout
 
 subscriber = mqtt.Client("Subscriber")
 
-# callback functions
 subscriber.on_message = on_message
 subscriber.on_connect = on_connect
 
