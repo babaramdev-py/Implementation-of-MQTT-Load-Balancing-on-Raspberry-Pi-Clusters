a211f375e2f515d45ec0bc378c92b8f6cd603da7
Ved K <vedkorla300@gmail.com>
2024-05-13 02:09:27 +0530

    Added code snippets

---
+++ temp.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a=120912;
+    int b=4000;
+    cout << a + b << endl;
+    return 0;
+}
---
+++ temp3.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a=400;
+    int b=200;
+    cout << a + b << endl;
+    return 0;
+}
