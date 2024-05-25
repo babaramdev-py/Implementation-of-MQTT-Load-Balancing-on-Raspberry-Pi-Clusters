512f81b32b543525ca8beec6855329cd4f29dfe9
Ved K <vedkorla300@gmail.com>
2024-05-25 17:09:18 +0530

    set up init script

--- initialise.sh
+++ initialise.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Run c1.py in a new terminal
 gnome-terminal -- python3 c1.py &
 
@@ -9,4 +8,6 @@ gnome-terminal -- python3 c2.py &
 # Run c3.py in a new terminal
 gnome-terminal -- python3 c3.py 
 
+sleep 0.5
 
+gnome-terminal -- python3 master.py 
